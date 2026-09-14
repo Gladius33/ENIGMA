80e0f9a518be46dd8774b2b7bc55b79b5dc8e427
Sébastien TOUILLEUX <121196522+Gladius33@users.noreply.github.com>
2026-09-14 17:31:39 +0200

    feat(windows): add WinUI main window bootstrap

---
+++ desktop/windows/Enigma.Desktop.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace Enigma.Desktop.WinUI;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
